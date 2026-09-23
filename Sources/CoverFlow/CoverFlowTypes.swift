22a6fd9ef03f280dca4ed09dfd5887fcb9de454d
wichtelimwald <98843826+wichtelimwald@users.noreply.github.com>
2026-09-23 23:29:04 +0200

    feat: make CoverFlow horizontal spread configurable

--- Sources/CoverFlow/CoverFlowTypes.swift
+++ Sources/CoverFlow/CoverFlowTypes.swift
@@ -14,19 +14,29 @@ public struct CoverFlowTuning: Equatable, Sendable {
     public let perspective: CGFloat
     public let visibleRange: CGFloat
     public let flowHeightScale: CGFloat
+    /// Horizontal spread used by the atan-compressed stack position.
+    /// Defaults to the legacy engine constant so existing consumers do not move.
+    public let horizontalSpreadScale: CGFloat
+    /// Additional linear side shift applied as cards recede from the centre.
+    /// Defaults to the legacy engine constant so existing consumers do not move.
+    public let sideShiftScale: CGFloat
 
     public init(
         scaleReduction: CGFloat,
         maxTiltAngle: CGFloat,
         perspective: CGFloat,
         visibleRange: CGFloat,
-        flowHeightScale: CGFloat
+        flowHeightScale: CGFloat,
+        horizontalSpreadScale: CGFloat = CoverFlowLayoutStyle.baseStackTightness,
+        sideShiftScale: CGFloat = CoverFlowLayoutStyle.baseSideShiftScale
     ) {
         self.scaleReduction = scaleReduction
         self.maxTiltAngle = maxTiltAngle
         self.perspective = perspective
         self.visibleRange = visibleRange
         self.flowHeightScale = flowHeightScale
+        self.horizontalSpreadScale = horizontalSpreadScale
+        self.sideShiftScale = sideShiftScale
     }
 
     public static let `default` = CoverFlowTuning(
